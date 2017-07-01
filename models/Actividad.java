5710d5b5270e0238a25284e5a4c932d4576b60ed
tomasmalio <tomasmalio@gmail.com>
2017-07-01 18:16:19 -0300

    Modify the activity because of the recommendation of the teacher

--- models/Actividad.java
+++ models/Actividad.java
@@ -10,34 +10,143 @@ import persistence.ActividadAbm;
 public class Actividad {
 	private int numeroActividad;
 	private Vector<Profesor> 	profesores;
-	private int[][] 			horaInicio;
-	private int[][]				horaFin;
 	
-	public Actividad(int numeroActividad, Vector<Profesor> profesores, int dia, Date horaI, Date horaF) {
+	private int					lunesInicio;
+	private int					lunesFin;
+	private int					martesInicio;
+	private int					martesFin;
+	private int					miercolesInicio;
+	private int					miercolesFin;
+	private int					juevesInicio;
+	private int					juevesFin;
+	private int					viernesInicio;
+	private int					viernesFin;
+	private int					sabadoInicio;
+	private int					sabadoFin;
+	private int					domingoInicio;
+	private int					domingoFin;
+	
+	public Actividad(
+			int numeroActividad, 
+			Vector<Profesor> profesores, 
+			int lunesInicio, 
+			int LunesFin,
+			int martesInicio, 
+			int martesFin, 
+			int miercolesInicio,
+			int miercolesFin,
+			int juevesInicio,
+			int juevesFin,
+			int viernesInicio,
+			int viernesFin,
+			int sabadoInicio,
+			int sabadoFin,
+			int domingoInicio,
+			int domingoFin) {
 		super();
 		this.setNumeroActividad(numeroActividad);
 		this.setProfesores(profesores);
-		
-		horaInicio 	= new int[2][7];
-		Calendar horaDeInicio = GregorianCalendar.getInstance();
-		horaDeInicio.setTime(horaI);
-		horaInicio[1][dia] = horaDeInicio.get(Calendar.HOUR);
-		horaInicio[2][dia] = horaDeInicio.get(Calendar.MINUTE);
-		
-		horaFin		= new int[2][7];
-		Calendar horaDeFin = GregorianCalendar.getInstance();
-		horaDeFin.setTime(horaF); 
-		horaFin[1][dia] = horaDeFin.get(Calendar.HOUR);
-		horaFin[2][dia] = horaDeFin.get(Calendar.MINUTE);
+		this.setLunesInicio(lunesInicio);
+		this.setLunesFin(lunesFin);
+		this.setMartesInicio(martesInicio);
+		this.setMartesFin(martesFin);
+		this.setMiercolesInicio(miercolesInicio);
+		this.setMiercolesFin(miercolesFin);
+		this.setJuevesInicio(juevesInicio);
+		this.setJuevesFin(juevesFin);
+		this.setViernesInicio(viernesInicio);
+		this.setViernesFin(viernesFin);
+		this.setSabadoInicio(sabadoInicio);
+		this.setSabadoFin(sabadoFin);
+		this.setDomingoInicio(domingoInicio);
+		this.setDomingoFin(domingoFin);
 		
 		ActividadAbm.getInstancia().insert(this);
 	}
 	
-	public int[][] getHoraInicio() {
-		return horaInicio;
+	public int getLunesInicio() {
+		return lunesInicio;
 	}
-	public int[][] getHoraFin() {
-		return horaFin;
+	public void setLunesInicio(int lunesInicio) {
+		this.lunesInicio = lunesInicio;
+	}
+	public int getLunesFin() {
+		return lunesFin;
+	}
+	public void setLunesFin(int lunesFin) {
+		this.lunesFin = lunesFin;
+	}
+	public int getMartesInicio() {
+		return martesInicio;
+	}
+	public void setMartesInicio(int martesInicio) {
+		this.martesInicio = martesInicio;
+	}
+	public int getMartesFin() {
+		return martesFin;
+	}
+	public void setMartesFin(int martesFin) {
+		this.martesFin = martesFin;
+	}
+	public int getMiercolesInicio() {
+		return miercolesInicio;
+	}
+	public void setMiercolesInicio(int miercolesInicio) {
+		this.miercolesInicio = miercolesInicio;
+	}
+	public int getMiercolesFin() {
+		return miercolesFin;
+	}
+	public void setMiercolesFin(int miercolesFin) {
+		this.miercolesFin = miercolesFin;
+	}
+	public int getJuevesInicio() {
+		return juevesInicio;
+	}
+	public void setJuevesInicio(int juevesInicio) {
+		this.juevesInicio = juevesInicio;
+	}
+	public int getJuevesFin() {
+		return juevesFin;
+	}
+	public void setJuevesFin(int juevesFin) {
+		this.juevesFin = juevesFin;
+	}
+	public int getViernesInicio() {
+		return viernesInicio;
+	}
+	public void setViernesInicio(int viernesInicio) {
+		this.viernesInicio = viernesInicio;
+	}
+	public int getViernesFin() {
+		return viernesFin;
+	}
+	public void setViernesFin(int viernesFin) {
+		this.viernesFin = viernesFin;
+	}
+	public int getSabadoInicio() {
+		return sabadoInicio;
+	}
+	public void setSabadoInicio(int sabadoInicio) {
+		this.sabadoInicio = sabadoInicio;
+	}
+	public int getSabadoFin() {
+		return sabadoFin;
+	}
+	public void setSabadoFin(int sabadoFin) {
+		this.sabadoFin = sabadoFin;
+	}
+	public int getDomingoInicio() {
+		return domingoInicio;
+	}
+	public void setDomingoInicio(int domingoInicio) {
+		this.domingoInicio = domingoInicio;
+	}
+	public int getDomingoFin() {
+		return domingoFin;
+	}
+	public void setDomingoFin(int domingoFin) {
+		this.domingoFin = domingoFin;
 	}
 	public int getNumeroActividad() {
 		return numeroActividad;
