943b1cf854d34a7f463164290b8e18f20229a1b4
tomasmalio <tomasmalio@gmail.com>
2017-07-03 13:05:18 -0300

    Connection

--- persistence/PoolConnection.java
+++ persistence/PoolConnection.java
@@ -52,13 +52,9 @@ public class PoolConnection {
 			/**
 			 * Setting driver configuration
 			 */
-			Class.forName("com.mysql.jdbc.Driver");  
-			Connection con = DriverManager.getConnection("jdbc:mysql://35.162.112.46:3306/gimnasio","root","rexonS67"); 
-			/*
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             String dbConnectString = jdbc + servidor; 
             Connection con = DriverManager.getConnection (dbConnectString, usuario, password);
-            */
             return con;
 		} catch (SQLException e) {
 			System.out.println("Mensaje Error: " + e.getMessage());
